cf08a777168b1aed85eea8774f36aa160d2ddeaa
DennyTim <luckystudydanit@gmail.com>
2022-12-28 01:47:38 +0200

    Added base testing: - layout - handlers - http requests - added testing httpModule - added testing spinner (jasmine) - added testing spinner (jest) - added testing notification (jasmine) - added testing notification (jest)
    * fixed bug with accessor
    
    - added separation to components
    - added module and service
    - implemented template driven form
    - implemented reactive forms
    - added dynamic testing
    - added dynamic testing(2)
    - added password testing
    - added password testing(2)
    - added async validator
    - replaced validation
    - added testing btn state
    - added testing routing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,47 @@
-import { TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed
+} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { routes } from "./router/app-router.module";
+import { Router } from "@angular/router";
+import { AppModule } from "./app.module";
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+        AppModule,
+        RouterTestingModule.withRoutes(routes)
+      ]
     }).compileComponents();
   });
 
-  it('passes', () => {
-    expect(true).toBe(true);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    router = TestBed.inject(Router);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  describe('Routing', () => {
+    it('displays homepage at /', async () => {
+      await router.navigate([ '/' ]);
+      fixture.detectChanges();
+      const page = fixture.nativeElement.querySelector('[data-testid="home-page"]');
+      expect(page).toBeTruthy();
+    })
+
+    it('displays homepage at /signup', async () => {
+      await router.navigate([ '/signup' ]);
+      fixture.detectChanges();
+      const page = fixture.nativeElement.querySelector('[data-testid="sign-up-page"]');
+      expect(page).toBeTruthy();
+    })
   })
 });
